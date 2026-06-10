66a625c3d1b4234f113f681dcf199e7b714910f3
Dennis Dumin <164201997+DennisDumin@users.noreply.github.com>
2026-06-10 17:12:55 +0200

    chore: allow live server frontend origins

--- core/settings.py
+++ core/settings.py
@@ -92,7 +92,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = os.getenv(
     "CORS_ALLOWED_ORIGINS",
     "http://localhost:3000,http://127.0.0.1:3000,"
-    "http://localhost:5173,http://127.0.0.1:5173",
+    "http://localhost:5173,http://127.0.0.1:5173,"
+    "http://localhost:5500,http://127.0.0.1:5500,"
+    "http://localhost:5501,http://127.0.0.1:5501",
 ).split(",")
 
 CORS_ALLOW_CREDENTIALS = True
